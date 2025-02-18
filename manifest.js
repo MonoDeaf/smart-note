c8c50ce0b021d1090795f6d745cae2b6531d75ae
MonoDeaf <159107216+MonoDeaf@users.noreply.github.com>
2025-02-17 23:22:44 -0500

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "Smart Note",
+  "short_name": "SmartNote",
+  "start_url": "/smart-note/",
+  "display": "standalone",
+  "background_color": "#0a0a0a",
+  "theme_color": "#0a0a0a",
+  "icons": [
+    {
+      "src": "/smart-note/icons/icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/smart-note/icons/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
